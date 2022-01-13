da13b417ad50dd1b13d8796d60b02cdd3df75a1a
alexsincai <alex.sincai@yahoo.co.uk>
2022-01-13 14:16:53 +0200

    unused import

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const Post = () => {
     const [post, setPost] = useState({
