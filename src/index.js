a94a762a8c850e660ebea1756e28e5c0232244b0
alexsincai <alex.sincai@yahoo.co.uk>
2022-01-13 13:22:36 +0200

    format

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React, {StrictMode} from 'react';
-import { render } from 'react-dom';
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
 
 const App = () => {
-  return <>
-    <p>The application has been cleaned up of all the extraneous stuff you'll likely never use</p>
-  </>
-}
+    return (
+        <>
+            <p>
+                The application has been cleaned up of all the extraneous stuff
+                you'll likely never use
+            </p>
+        </>
+    );
+};
 
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById("root")
 );
